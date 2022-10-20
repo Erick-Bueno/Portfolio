f507ad23435c3de5b3bdd288b7d20f7f489b86dc
Erick Bueno <101439440+Erick-Bueno@users.noreply.github.com>
2022-10-19 22:38:50 -0300

    resposividade melhorada

--- animacoes.js
+++ animacoes.js
@@ -2,6 +2,14 @@ const ele = document.querySelector(".imgi2")
 const mobi = document.querySelector("#open-mob")
 const over = document.querySelector(".overlay")
 const fechar = document.querySelector(".fecharr")
+const l1 = document.querySelector("#link1")
+const l2 = document.querySelector("#link2")
+const l3 = document.querySelector("#link3")
+const l4 = document.querySelector("#link4")
+const borda1 = document.querySelector(".borda1")
+const borda2 = document.querySelector(".borda2")
+const borda3 = document.querySelector(".borda3")
+const borda4 = document.querySelector(".borda4")
 console.log(ele)
 window.addEventListener("scroll", function(){
     const distancia_do_scroll_pro_topo_da_Pagina = window.pageYOffset + ((window.innerHeight * 3)/4);
@@ -16,6 +24,44 @@ window.addEventListener("scroll", function(){
 mobi.addEventListener("click", function(){
     over.style.width = "55%"
 })
+
+
+
 fechar.addEventListener("click", function(){
     over.style.width = "0%"
+    borda1.style.width = "0%"
+    borda2.style.width = "0%"
+    borda3.style.width = "0%"
+    borda4.style.width = "0%"
 })
+l1.addEventListener("click", function(){
+    borda1.style.width ="80px"
+    borda2.style.width = "0%"
+    borda3.style.width = "0%"
+    borda4.style.width = "0%"
+})
+l2.addEventListener("click", function(){
+    borda1.style.width ="0%"
+    borda2.style.width = "125px"
+    borda3.style.width = "0%"
+    borda4.style.width = "0%"
+    
+  
+})
+l3.addEventListener("click", function(){
+    borda1.style.width ="0%"
+    borda2.style.width = "0%"
+    borda3.style.width = "120px"
+    borda4.style.width = "0%"
+    
+  
+})
+l4.addEventListener("click", function(){
+    borda1.style.width ="0%"
+    borda2.style.width = "0%"
+    borda3.style.width = "0%"
+    borda4.style.width = "140px"
+    
+  
+})
+
